42577c289786b36136060ca1068cd2e1249feb02
DeBellotte <jdebello@innovativetech.com>
2022-09-20 16:28:58 -0400

    feat/change h2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Keyring's React Challenge 🚀</h1>
-      <h2>Done by YOUR NAME HERE</h2>
+      <h2>FolaDeb</h2>
       <h3>19. Sept. 2022</h3>
       <Button variant="contained">FORK THIS SANDBOX</Button>
     </div>
